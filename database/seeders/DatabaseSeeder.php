1f6d36284a0758d124a33fa9215c8aa68ccc25e6
muhammadedowardaya <muhammadedowarday4@gmail.com>
2021-11-20 22:49:24 -0800

    commit semua changes

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\{Category, User, Post};
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory(3)->create();
+        Post::factory(20)->create();
+
+        Category::create([
+            'name' => 'Programming',
+            'slug' => 'programming'
+        ]);
+
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design'
+        ]);
+    }
+}
